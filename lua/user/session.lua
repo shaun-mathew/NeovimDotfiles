6c95196376b63fb46524fc9a313aafc787eee321
Shaun Mathew <shaunmathew123@gmail.com>
2022-03-26 14:48:33 -0400

    Added hooks to close problematic windows and buffers to avoid conflicting with auto-session

--- lua/user/session.lua
+++ lua/user/session.lua
@@ -3,7 +3,22 @@ if not status_ok then
 	return
 end
 
+local function close_neogit()
+    if vim.bo.filetype == "NeogitStatus" then
+        require("neogit").close()
+    end
+end
+
+local function close_zenmode()
+    if ZEN_ACTIVE then
+        require("zen-mode").toggle()
+    end
+end
+
 session.setup({
     log_level = 'info',
-    auto_session_suppress_dirs = {"~/", "~/Projects", "~/PycharmProjects"}
+    auto_session_suppress_dirs = {"~/", "~/Projects", "~/PycharmProjects"},
+    pre_save_cmds = {close_neogit, "NvimTreeClose", "TroubleClose", "SymbolsOutlineClose", close_zenmode}
+
 })
+
--- lua/user/zen.lua
+++ lua/user/zen.lua
@@ -1,6 +1,13 @@
 local status_ok, zen = pcall(require, "zen-mode")
+ZEN_ACTIVE = false
 if not status_ok then
-  return
+	return
 end
 
-zen.setup({})
+zen.setup({
+	on_open = function(_) ZEN_ACTIVE = true
+    end,
+
+    on_close = function() ZEN_ACTIVE = false
+    end
+})
