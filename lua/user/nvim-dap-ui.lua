00ab0a104b7378f4a507ef44fb6b811a6b1ceca6
shaun.mathew <shaunmathew123@gmail.com>
2022-03-27 00:52:33 -0400

    Open and close dapui on debugger start and stop events

--- lua/user/nvim-dap-ui.lua
+++ lua/user/nvim-dap-ui.lua
@@ -4,3 +4,13 @@ if not status_ok then
 end
 
 nvim_dap_ui.setup()
+local dap = require("dap")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  nvim_dap_ui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  nvim_dap_ui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  nvim_dap_ui.close()
+end
